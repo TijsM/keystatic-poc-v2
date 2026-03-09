891e5997b7595f158c746cb40f31e77537fd7f9d
Tijs Martens <tijs.martens.tijs@gmail.com>
2026-03-09 18:24:11 +0100

    fix: com as sitemap url

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
-import type { MetadataRoute } from 'next';
-import { reader } from './reader';
+import type { MetadataRoute } from "next";
+import { reader } from "./reader";
 
-const BASE_URL = 'https://rodi-sites.nl';
+const BASE_URL = "https://rodi-sites.com";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const pageSlugs = await reader.collections.pages.list();
@@ -10,13 +10,13 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     {
       url: BASE_URL,
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "weekly",
       priority: 1,
     },
     ...pageSlugs.map((slug) => ({
       url: `${BASE_URL}/${slug}`,
       lastModified: new Date(),
-      changeFrequency: 'monthly' as const,
+      changeFrequency: "monthly" as const,
       priority: 0.8,
     })),
   ];
